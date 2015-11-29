9079e17151123c3cdb0f0e2c1dbacf3cd3d10727
apoorvmahajan <apoorv.mahajan09@gmail.com>
2015-11-28 20:28:41 -0500

    Added Disable screenlock

--- build-vm/commands.sh
+++ build-vm/commands.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Disabling Lock Screen..."
+/usr/bin/gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+/usr/bin/gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+echo "Preparation for running the tool"
 cd /home/vagrant/Desktop/AaltaSourceCode/FSE-2014-Aalta/
 sudo make
 echo "Make Complete, now running tool"
